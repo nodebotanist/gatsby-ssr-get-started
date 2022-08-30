1a5e60d92befe5e9f4ab5de6bcd0dab1bf050e4b
Kassian Wren <the@nodebotani.st>
2022-08-30 14:49:19 -0500

    Completed all steps up to deploy

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -4,7 +4,7 @@ const Page = ({serverData}) => {
     return <main>
         <h1>Hello from the server!</h1>
         <pre>{JSON.stringify(serverData, null, 2)}</pre>
-        <img src={`https://cataas.com${serverData.cat.url}`} />
+        {serverData.cat.img}
     </main>
 }
 
@@ -12,6 +12,7 @@ export default Page
 
 export async function getServerData() {
     let cat = await (await fetch('https://cataas.com/cat?json=true')).json()
+    cat.img = <img src={`https://cataas.com${cat.url}`} />
     return { props: {
         cat
     }}
