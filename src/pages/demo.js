b99ba669aedda2524438824aa90e243c1a142b64
Kassian Wren <the@nodebotani.st>
2022-08-30 14:47:34 -0500

    Added server rendered URL

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -1,14 +1,18 @@
 import React from "react"
 
-const Page = (serverData) => {
+const Page = ({serverData}) => {
     return <main>
         <h1>Hello from the server!</h1>
         <pre>{JSON.stringify(serverData, null, 2)}</pre>
+        <img src={`https://cataas.com${serverData.cat.url}`} />
     </main>
 }
 
 export default Page
 
 export async function getServerData() {
-    return { props: {}}
+    let cat = await (await fetch('https://cataas.com/cat?json=true')).json()
+    return { props: {
+        cat
+    }}
 }
